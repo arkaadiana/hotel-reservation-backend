bb8a5a65cac49f347d295041c68ace46333b57ad
arkaadiana <220040193@stikom-bali.ac.id>
2024-06-03 13:58:27 +0800

    Update function addCustomers and Added function updateCustomers in CustomersController.php

--- app.php
+++ app.php
@@ -9,4 +9,6 @@ $db = $database->getConnection();
 $router = new Router();
 $router->register('GET', '/api/customers', [new CustomersController($db), 'readCustomers']);
 $router->register('POST', '/api/customers', [new CustomersController($db), 'addCustomers']);
+$router->register('PUT', '/api/customers', [new CustomersController($db), 'updateCustomers']);
 $router->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
