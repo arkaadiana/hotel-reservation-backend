944f783839a8ae89bc29a45c0e2edd83e3e0a891
arkaadiana <220040193@stikom-bali.ac.id>
2024-06-04 12:26:59 +0800

    Added DELETE method support to router for CustomersController

---
+++ app.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'controllers/CustomersController.php';
+include_once 'config/database.php'; 
+include_once 'middleware/Router.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$router = new Router();
+$router->register('GET', '/api/customers', [new CustomersController($db), 'readCustomers']);
+$router->register('POST', '/api/customers', [new CustomersController($db), 'addCustomers']);
+$router->register('PUT', '/api/customers', [new CustomersController($db), 'updateCustomers']);
+$router->register('DELETE', '/api/customers', [new CustomersController($db), 'deleteCustomers']);
+
+$router->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
