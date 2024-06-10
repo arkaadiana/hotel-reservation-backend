b7c038062e97b4832452852262fbf2db28f688e8
arkaadiana <220040193@stikom-bali.ac.id>
2024-06-10 23:37:15 +0800

    Added routing for TransactionsController endpoints to handle CRUD operations

--- app.php
+++ app.php
@@ -1,6 +1,8 @@
 <?php
 include_once 'controllers/CustomersController.php';
 include_once 'controllers/RoomsController.php';
+include_once 'controllers/TransactionsController.php';
+
 include_once 'config/database.php'; 
 include_once 'middleware/Router.php';
 
@@ -18,5 +20,10 @@ $router->register('GET', '/api/rooms/status', [new RoomsController($db), 'checkR
 $router->register('POST', '/api/rooms', [new RoomsController($db), 'addRooms']);
 $router->register('PUT', '/api/rooms', [new RoomsController($db), 'updateRooms']);
 $router->register('DELETE', '/api/rooms', [new RoomsController($db), 'deleteRooms']);
+
+$router->register('GET', '/api/transactions', [new TransactionsController($db), 'readTransactions']);
+$router->register('POST', '/api/transactions', [new TransactionsController($db), 'addTransactions']);
+$router->register('PUT', '/api/transactions', [new TransactionsController($db), 'updateTransactions']);
+$router->register('DELETE', '/api/transactions', [new TransactionsController($db), 'deleteTransactions']);
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
